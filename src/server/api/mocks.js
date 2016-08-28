503fc4700e263821e56ff4eb2472ad9005cd06af
Cathy Zoller <cathy@mojotech.com>
2016-08-28 07:37:06 -0600

    initial mockup of dog-treat data

--- src/server/api/mocks.js
+++ src/server/api/mocks.js
@@ -1,6 +1,31 @@
+import { MockList } from 'graphql-tools';
+import casual from 'casual';
+
 const mocks = {
-  String: () => 'It works!',
-  Int: () => 42
-}
+  Int: () => casual.integer(1,1000),
+  allOwners: () => ({
+    owners: () => new MockList([1,10])
+  }),
+  Owner: () => ({
+    firstName: () => casual.first_name,
+    lastName: () => casual.last_name,
+    dogs: () => new MockList([1,3])
+  }),
+  Dog: () => ({
+    name: () => casual.word,
+    treats: () => new MockList([1,6], () => casual.word)
+  }),
+  Treat: () => ({
+    name: () => casual.word
+  }),
+  RootQuery: () => ({
+    owner: (o, args) => {
+      if (casual.integer(1,10) > 8){
+        return null;
+      }
+      return { ...args };
+    },
+  }),
+};
 
 export default mocks
--- src/server/api/schema.js
+++ src/server/api/schema.js
@@ -2,8 +2,35 @@ import { schema as countSchema, resolvers as countResolvers } from './count'
 import Data from './data'
 
 const rootSchema = `
+  type allOwners {
+    owners: [Owner]
+  }
+
+  type Owner {
+    id: Int! # the ! means that every owner object _must_ have an id
+    firstName: String
+    lastName: String
+    allOwners: allOwners
+    dogs: [Dog]
+  }
+
+  type Dog {
+    id: Int!
+    name: String
+    treats: [Treat]
+    owner: Owner
+  }
+
+  type Treat {
+    id: Int!
+    name: String
+  }
+
   type RootQuery {
     count: Count
+    allOwners: allOwners
+    owner: Owner
+    dog: Dog
   }
 
   type RootMutation {
