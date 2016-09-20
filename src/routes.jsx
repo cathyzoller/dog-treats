1354e8db391dccba54561aa4bdb3e4f7efc12439
Cathy Zoller <cathy@mojotech.com>
2016-09-20 06:08:24 -0600

    hook dogtreats to store

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,15 @@
 import { Route, IndexRedirect } from 'react-router'
 import LocalCounter from './containers/LocalCounter'
 import RemoteCounter from './containers/RemoteCounter'
+import DogtreatsContainer from './containers/DogtreatsContainer'
 import App from './components/App'
 import React from 'react'
 
 export default (
   <Route path='/' component={App}>
-    <IndexRedirect to='/remote-counter' />
+    <IndexRedirect to='/dog-treats' />
     <Route path='local-counter' component={LocalCounter} />
     <Route path='remote-counter' component={RemoteCounter} />
+    <Route path='dog-treats' component={DogtreatsContainer} />
   </Route>
 )
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,8 +1,16 @@
 export const ADD_COUNT = 'ADD_COUNT'
+export const ADD_TREAT = 'ADD_TREAT'
 
-export function addCount(amount) {
-  return {
-    type: ADD_COUNT,
-    payload: amount
-  }
+export const addCount = (amount) => {
+  return { type: ADD_COUNT, amount }
 }
+
+export const setTreat = (name) => (
+  (dispatch, getState) => {
+    console.log('name', name);
+    console.log('state', getState().apollo.data);
+    //const { treats } = getState().apollo.data.ROOT_QUERY.allTreats;
+    //const newTreats = treats.push({ name: name });
+    //return { type: ADD_TREAT, newTreats };
+  }
+);
