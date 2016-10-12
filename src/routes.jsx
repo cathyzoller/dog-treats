6ab300bdde45c2031a82e1b81bbd2d16c384960b
Cathy Zoller <cathy@mojotech.com>
2016-10-12 16:02:40 -0600

    remove unused starter files

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,4 @@
 import { Route, IndexRedirect } from 'react-router'
-import LocalCounter from './containers/LocalCounter'
-import RemoteCounter from './containers/RemoteCounter'
 import DogtreatsContainer from './containers/DogtreatsContainer'
 import App from './components/App'
 import React from 'react'
@@ -8,8 +6,6 @@ import React from 'react'
 export default (
   <Route path='/' component={App}>
     <IndexRedirect to='/dog-treats?language=en' />
-    <Route path='local-counter' component={LocalCounter} />
-    <Route path='remote-counter' component={RemoteCounter} />
     <Route path='dog-treats' component={DogtreatsContainer} />
   </Route>
 )
