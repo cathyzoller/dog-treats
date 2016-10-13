3afbdc925371d4500c70ed2a9752ffc6dc32af0f
Cathy Zoller <cathy@mojotech.com>
2016-10-13 11:42:17 -0600

    change translation directory

--- src/utils/translator.js
+++ src/utils/translator.js
@@ -4,7 +4,7 @@ import { sync as globSync } from 'glob';
 import { sync as mkdirpSync } from 'mkdirp';
 
 const filePattern = './build/messages/**/*.json';
-const outputLanguageDataDir = './build/locales/';
+const outputLanguageDataDir = './locales/';
 
 // Aggregates the default messages that were extracted from the example app's
 // React components via the React Intl Babel plugin. An error will be thrown if
