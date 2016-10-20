771dfcdc00c477fd2575fc8382e15dc2b9069e5f
Cathy Zoller <cathy@mojotech.com>
2016-10-20 14:18:28 -0600

    removed setTreat action by mistake

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,13 +4,6 @@ import { localeData } from '../../locales/data.js'
 export const SET_LOCALE = 'SET_LOCALE'
 export const ADD_TREAT = 'ADD_TREAT'
 
-export const setTreat = (name) => (
-  (dispatch, getState) => {
-    // const { treats } = getState().apollo.data.ROOT_QUERY.allTreats;
-    // const newTreats = treats.push({ name: name });
-    // return { type: ADD_TREAT, newTreats };
-  }
-)
 
 export const setLocale = (locale) => (
   (dispatch) => {
@@ -22,3 +15,13 @@ export const setLocale = (locale) => (
     return { type: SET_LOCALE, locale, messages }
   }
 )
+
+export const setTreat = (name) => (
+  (dispatch, getState) => {
+    console.log('name', name);
+    console.log('state', getState().apollo.data);
+    //const { treats } = getState().apollo.data.ROOT_QUERY.allTreats;
+    //const newTreats = treats.push({ name: name });
+    //return { type: ADD_TREAT, newTreats };
+  }
+);
